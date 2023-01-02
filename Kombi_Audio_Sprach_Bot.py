5a5f09e29da8e775a0c036d76368bcfe03596b70
SvenjaCSch <73709681+SvenjaCSch@users.noreply.github.com>
2023-01-02 13:13:39 +0100

    Add files via upload

---
+++ Kombi_Audio_Sprach_Bot.py
@@ -0,0 +1,55 @@
+import random
+import datetime
+import webbrowser
+import pyttsx3
+import wikipedia
+from pygame import mixer
+import speech_recognition as sr
+import pandas as pd
+import numpy as np
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+import time
+time.clock = time.time
+
+import yaml
+import os
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+volume = engine.getProperty('volume')
+engine.setProperty('volume', 10.0)
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate - 25)
+
+chatbot = ChatBot("Speaker")
+trainer = ListTrainer(chatbot)
+
+trainer.train(
+    'C:/Users/svcsc/Desktop/Code/archive'
+)
+
+exit_conditions = (":q", "quit", "exit", "bye", "ciao", "see you", "I have to go")
+
+while True:
+    now = datetime.datetime.now()
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("What do you what to know?")
+        audio = r.listen(source)
+        try:
+            said_words = r.recognize_google(audio)
+            print("You said:- " + said_words)
+            if said_words in exit_conditions:
+                break
+            else:
+                print(f"{chatbot.get_response(said_words)}")
+        except sr.UnknownValueError:
+            print("Could not understand a word.")
+            engine.say('I didnt get that. Rerun the code')
+            engine.runAndWait()
+
+    
